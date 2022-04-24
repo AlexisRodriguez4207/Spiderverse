6c24c3553892763aa88057968b6c0dea52bb164d
Xero4207 <carlos.rodriguez201@tectijuana.edu.mx>
2022-04-23 23:00:15 -0700

    Agregando Pruebas de Spiderman y Resultados Esperados

--- Test/Spiderman.test.js
+++ Test/Spiderman.test.js
@@ -1,6 +1,24 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () =>{
-        const resultOfSomething = 1+2
-        expect(resultOfSomething).toBe(18);
+const Spiderman = require('./../App/Spiderman')
+
+describe("Unit Tests for Spiderman Class", () => {
+    test('1) Create an Spiderman object', () =>{
+
+        // Aqui escribimos el codigo que queremos usar tal cual
+        // Quiero poder instanciar un objeto Spiderman con esta informacion
+        const AndrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield",31, "Sony")
+
+        // Validamos que este codigo funcione de al forma esperada
+        expect(AndrewGarfield.name).toBe("Spiderman Sony")
+        expect(AndrewGarfield.age).toBe(31)
+        expect(AndrewGarfield.actor).toBe("Andrew Garfield")
+        expect(AndrewGarfield.movies).toBe(31)
+        expect(AndrewGarfield.studio).toBe("Sony")
+    });
+
+    test('2) Use the method getInfo()', () =>{
+        const TomHolland = new Spiderman ("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+
+        // Validacion del Codigo
+        expect(TomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
     });
 })
